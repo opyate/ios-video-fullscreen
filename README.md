6a22cd7f6781d67024b4d40bda1a69119059f5ea
Juan M Uys <opyate@gmail.com>
2021-05-14 16:22:04 +0100

    add example with one video tag

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Intro
+# One
 
-Like 0003, but trying some iOS-specific changes.
+When one `video` and one `canvas` element is on the page, and you request fullscreen on the `canvas` element on iOS, it will fullscreen the `video` element.
 
-# Conclusion
+# Two
 
-Using an iDevice on browserstack maximises the video element, not the canvas as requested in the Javascript.
+When two of each is on the page, then fullscreen works for no element on iOS.
 
-I suggest getting a cheap iDevice on eBay so I can debug this in more depth, as browserstack limits sessions to 30 seconds, after which you can't use a specific device again.
+# Notes
+
+Fullscreen canvas will have a green border to distinguish it from the video.
+
+Tested on simulator with `iPhone XR - 12.4`.
---
+++ one.js
@@ -0,0 +1,41 @@
+const video1 = document.getElementById('video1');
+const canvas1 = document.getElementById('canvas1');
+const context1 = canvas1.getContext('2d');
+
+const renderVideo = () => {
+    // canvas1.width = video1.videoWidth;
+    // canvas1.height = video1.videoHeight;
+    context1.drawImage(video1, 0, 0, video1.videoWidth, video1.videoHeight);
+}
+
+const tick = () => {
+    renderVideo();
+    requestAnimationFrame(tick);
+}
+
+window.requestAnimationFrame(tick)
+
+function fullscreen(elementId) {
+    let elementToFullscreen;
+
+    switch (elementId) {
+        case "video1": elementToFullscreen = video1; break;
+        case "canvas1": elementToFullscreen = canvas1; break;
+    }
+
+    if (elementToFullscreen.requestFullscreen) {
+        elementToFullscreen.requestFullscreen();
+    } else if (elementToFullscreen.webkitRequestFullscreen) {
+        elementToFullscreen.webkitRequestFullscreen();
+    } else if (elementToFullscreen.msRequestFullScreen) {
+        elementToFullscreen.msRequestFullScreen();
+    }
+}
+
+document.addEventListener('click', (event) => {
+    const btn = event.target;
+    // only listen to buttons
+    if (btn.tagName.toLowerCase() !== "button") return;
+    const id = btn.dataset.elementid;
+    fullscreen(id);
+})
